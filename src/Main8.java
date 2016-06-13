ecb19ed467e9fb2b1d38a3f98a7adde3db0c6f92
Remzi <remzitdf@abv.bg>
2016-06-13 16:58:48 +0300

    ok

---
+++ src/Main8.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main8 {
+
+	public static void main(String[] args)throws java.lang.Exception {
+		int a[] = { 1, 2, 3, 3, 2, 1 };
+		System.out.println(Arrays.toString(a));
+		System.out.println("Size is: " + a.length);
+		boolean symmetric = true;
+				
+		for (int i = 0; i < a.length / 2; i++) {
+			if (a[i] != a[a.length - i - 1]) { 																						// елементи
+				symmetric =false;
+				break;
+			}
+		}		
+				System.out.println("Symmetric: " + symmetric + "!");
+	}
+}
