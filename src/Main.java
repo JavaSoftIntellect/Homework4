de9ba6a8e6adb59f4c35c2e8c8915c32d3226774
Remzi <remzitdf@abv.bg>
2016-06-13 16:49:51 +0300

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 
 public class Main {
 
+
 	public static void main(String[] args) {
-		int[] array = new int[] {-2,4,6,7,9,12};
-		int sum=0;
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] % 2 == 0) {
-				sum += array[i];
+		System.out.println("Array:");
+		int a[] = { 8,2,14,9,22,5 };
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		int n = a.length;
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+
+				if (a[j - 1] > a[j]) {
+					temp = a[j - 1];
+					a[j - 1] = a[j];
+					a[j] = temp;
+				}
 			}
-			System.out.println("The sum of the odd numbers is:" + sum);
+		}
+		System.out.println();
+		System.out.println("Bubble Sort:");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+
 		}
 	}
-
 }
